af7eb9bfcfab5389c6e6110d2605d98009e2c4ef
Alexis Gommet <sherman503@hotmail.fr>
2022-09-04 00:51:23 +0200

    Initial Commit

---
+++ components/DatePicker.tsx
@@ -0,0 +1,78 @@
+import type { Dayjs } from 'dayjs';
+import type { FC } from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+import { LocalizationProvider, StaticDatePicker } from '@mui/x-date-pickers';
+import styles from '../styles/DatePicker.module.css';
+
+interface Props {
+    date: Dayjs | null,
+    setDate: (newValue: Dayjs | null) => void
+    hour: number,
+    setHour: (newValue: number) => void,
+    channel: string,
+    setChannel: (newValue: string) => void,
+}
+
+const DatePicker: FC<Props> = ({ date, setDate, hour, setHour, channel, setChannel }) => {
+
+    const channelButtons = ['cnn', 'fox'];
+    const hourButtons = [
+        {hour: 0, hourString: '12AM'}, 
+        {hour: 6, hourString: '6AM'}, 
+        {hour: 12, hourString: '12PM'}, 
+        {hour: 18, hourString: '6PM'}
+    ];
+
+    const currentDate = new Date().toLocaleString('sv', {timeZone: 'Europe/Paris'});
+    const currentHour = parseInt(currentDate.split(' ')[1].split(':')[0]);
+    const currentDay = currentDate.split(' ')[0];
+
+    return (
+        <>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <StaticDatePicker
+                    displayStaticWrapperAs="desktop"
+                    openTo='day'
+                    minDate={dayjs('2022-08-29')}
+                    inputFormat='YYYY-MM-DD'
+                    disableFuture={true}
+                    value={date}
+                    onChange={(newValue) => setDate(newValue)}
+                    renderInput={(params) => <>{params}</>}
+                />
+            </LocalizationProvider>
+            <div className={styles.channelPicker}>
+                <div className={styles.channelPickerFlex}>
+                    {channelButtons.map((item, index) => {
+                        return <button 
+                            key={index} 
+                            className={channel === item ? styles.active : ''} 
+                            onClick={() => setChannel(item)}
+                        >
+                            {item.toUpperCase()}
+                        </button>
+                    })}
+                </div>
+                <div className={styles.channelSlider}></div>
+            </div>
+            <div className={styles.hourPicker}>
+                <div className={styles.hourPickerFlex}>
+                    {hourButtons.map((item, index) => {
+                        return <button 
+                            key={index} 
+                            className={hour === item.hour ? styles.active : ''} 
+                            onClick={() => setHour(item.hour)}
+                            disabled={currentHour < item.hour && date?.format('YYYY-MM-DD') === currentDay}
+                        >
+                            {item.hourString}
+                        </button>
+                    })}
+                </div>
+                <div className={styles.hourSlider}></div>
+            </div>
+        </>
+    )
+}
+
+export default DatePicker;
---
+++ functions/src/index.ts
@@ -0,0 +1,86 @@
+import * as path from'path' ;
+import * as os from 'os';
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { Storage } from '@google-cloud/storage';
+import * as puppeteer from 'puppeteer';
+import type { Browser, Page } from 'puppeteer';
+
+admin.initializeApp();
+
+const TIMEOUT = 60 * 1000;
+const TMPDIR = os.tmpdir();
+
+exports.puppeteerCronjob = functions.runWith({ timeoutSeconds: 150, memory: '2GB' }).pubsub.schedule('0 */6 * * *').timeZone('Europe/Paris').onRun(async () => {
+    const date = getDate();    
+    await getScreenshots();
+    await uploadScreenshots(date);
+});
+
+function getDate(){
+    const date = new Date().toLocaleString('sv', {timeZone: 'Europe/Paris'});
+    return `${date.split(' ')[0]}-${date.split(' ')[1].split(':')[0]}`; 
+}
+
+async function getScreenshots(){
+    const browser = await puppeteer.launch();
+    await getScreenshot(browser, 'https://edition.cnn.com/', 'CNN');
+    await getScreenshot(browser, 'https://www.foxnews.com/', 'FOX');
+    await browser.close();
+}
+
+async function uploadScreenshots(timestamp: string){
+    const storage = new Storage();
+    const bucket = storage.bucket('us-media-archive.appspot.com');  
+    await bucket.upload(path.resolve(`${TMPDIR}/CNN.jpg`), { destination: `images/CNN/${timestamp}.jpg` });
+    await bucket.upload(path.resolve(`${TMPDIR}/FOX.jpg`), { destination: `images/FOX/${timestamp}.jpg` });
+}
+
+async function getScreenshot(browser: Browser, url: string, name: string){
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(TIMEOUT);
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    });  
+    await page.goto(url);
+    await scrollToBottom(page);
+    await waitForImagesToLoad(page);
+    await page.screenshot({
+        path: `${TMPDIR}/${name}.jpg`,
+        fullPage: true,
+        type: 'jpeg'
+    });
+    await page.close();
+}
+
+async function waitForImagesToLoad(page: Page){
+    await page.evaluate(async () => {
+        const selectors = Array.from(document.querySelectorAll("img"));
+        await Promise.all(selectors.map(img => {
+            if (img.complete) return;
+            return new Promise((resolve, reject) => {
+                img.addEventListener('load', resolve);
+                img.addEventListener('error', reject);
+            });
+        }));
+    });
+}
+
+async function scrollToBottom(page: Page){
+    await page.evaluate(async () => {
+        await new Promise<void>((resolve) => {
+            let totalHeight = 0;
+            const distance = 100;
+            let timer = setInterval(() => {
+                let scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
